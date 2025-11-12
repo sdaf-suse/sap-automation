f7995c3d6236d07fb730d47690d35b7b0ecd4c8b
hdamecharla <hdamecharla@microsoft.com>
2025-11-12 21:10:43 +0530

    terraform module changes for SAP control plane including deployer and library

--- deploy/terraform/terraform-units/modules/sap_deployer/app_service.tf
+++ deploy/terraform/terraform-units/modules/sap_deployer/app_service.tf
@@ -12,23 +12,23 @@ resource "azurerm_subnet" "webapp" {
                                                     azurerm_subnet.subnet_mgmt
                                                   ]
 
-  count                                         = var.use_webapp ? local.webapp_subnet_exists ? 0 : 1 : 0
+  count                                         = var.app_service.use ? var.infrastructure.virtual_network.management.subnet_webapp.exists ? 0 : 1 : 0
   name                                          = local.webapp_subnet_name
-  resource_group_name                           = local.management_virtual_network_exists ? (
+  resource_group_name                           = var.infrastructure.virtual_network.management.exists ? (
                                                     data.azurerm_virtual_network.vnet_mgmt[0].resource_group_name) : (
                                                     azurerm_virtual_network.vnet_mgmt[0].resource_group_name
                                                   )
-  virtual_network_name                          = local.management_virtual_network_exists ? (
+  virtual_network_name                          = var.infrastructure.virtual_network.management.exists ? (
                                                     data.azurerm_virtual_network.vnet_mgmt[0].name) : (
                                                     azurerm_virtual_network.vnet_mgmt[0].name
                                                   )
 
-  address_prefixes                              = [local.webapp_subnet_prefix]
+  address_prefixes                              = [var.infrastructure.virtual_network.management.subnet_webapp.prefix]
 
   private_endpoint_network_policies             = var.use_private_endpoint ? "Enabled" : "Disabled"
 
   service_endpoints                             = var.use_service_endpoint ? (
-                                                    var.use_webapp ? (
+                                                    var.app_service.use ? (
                                                       ["Microsoft.Storage", "Microsoft.KeyVault", "Microsoft.Web"]) : (
                                                       ["Microsoft.Storage", "Microsoft.KeyVault"]
                                                     )) : (
@@ -36,7 +36,7 @@ resource "azurerm_subnet" "webapp" {
                                                   )
 
   dynamic "delegation" {
-                        for_each = range(var.use_webapp ? 1 : 0)
+                        for_each = range(var.app_service.use ? 1 : 0)
                         content {
                           name = "delegation"
                           service_delegation {
@@ -49,25 +49,25 @@ resource "azurerm_subnet" "webapp" {
 }
 
 data "azurerm_subnet" "webapp" {
-  count                                         = var.use_webapp ? local.webapp_subnet_exists ? 1 : 0 : 0
-  name                                          = split("/", local.webapp_subnet_arm_id)[10]
-  resource_group_name                           = split("/", local.webapp_subnet_arm_id)[4]
-  virtual_network_name                          = split("/", local.webapp_subnet_arm_id)[8]
+  count                                         = var.app_service.use ? var.infrastructure.virtual_network.management.subnet_webapp.exists ? 1 : 0 : 0
+  name                                          = split("/", var.infrastructure.virtual_network.management.subnet_webapp.id)[10]
+  resource_group_name                           = split("/", var.infrastructure.virtual_network.management.subnet_webapp.id)[4]
+  virtual_network_name                          = split("/", var.infrastructure.virtual_network.management.subnet_webapp.id)[8]
 }
 
 
 
 # Create the Windows App Service Plan
 resource "azurerm_service_plan" "appserviceplan" {
-  count                                         = var.use_webapp ? 1 : 0
+  count                                         = var.app_service.use ? 1 : 0
   name                                          = lower(format("%s%s%s%s",
                                                     var.naming.resource_prefixes.app_service_plan,
                                                     var.naming.prefix.DEPLOYER,
                                                     var.naming.resource_suffixes.app_service_plan,
                                                     coalesce(try(var.infrastructure.custom_random_id, ""), substr(random_id.deployer.hex, 0, 3)))
                                                   )
-  resource_group_name                           = local.resourcegroup_name
-  location                                      = local.rg_appservice_location
+  resource_group_name                           = var.infrastructure.resource_group.exists ? data.azurerm_resource_group.deployer[0].name : azurerm_resource_group.deployer[0].name
+  location                                      = var.infrastructure.resource_group.exists ? data.azurerm_resource_group.deployer[0].location : azurerm_resource_group.deployer[0].location
   os_type                                       = "Windows"
   sku_name                                      = var.deployer.app_service_SKU
   tags                                          = var.infrastructure.tags
@@ -76,15 +76,15 @@ resource "azurerm_service_plan" "appserviceplan" {
 
 # Create the app service with AD authentication and storage account connection string
 resource "azurerm_windows_web_app" "webapp" {
-  count                                          = var.use_webapp ? 1 : 0
+  count                                          = var.app_service.use ? 1 : 0
   name                                           = lower(format("%s%s%s%s",
                                                     var.naming.resource_prefixes.app_service_plan,
                                                     var.naming.prefix.LIBRARY,
                                                     var.naming.resource_suffixes.webapp_url,
                                                     coalesce(try(var.infrastructure.custom_random_id, ""), substr(random_id.deployer.hex, 0, 3)))
                                                     )
-  resource_group_name                            = local.resourcegroup_name
-  location                                       = local.rg_appservice_location
+  resource_group_name                            = var.infrastructure.resource_group.exists ? data.azurerm_resource_group.deployer[0].name : azurerm_resource_group.deployer[0].name
+  location                                       = var.infrastructure.resource_group.exists ? data.azurerm_resource_group.deployer[0].location : azurerm_resource_group.deployer[0].location
   service_plan_id                                = azurerm_service_plan.appserviceplan[0].id
   https_only                                     = true
   webdeploy_publish_basic_authentication_enabled = false
@@ -101,12 +101,13 @@ resource "azurerm_windows_web_app" "webapp" {
   #   unauthenticated_client_action = "RedirectToLoginPage"
   # }
 
-  tags                                          = var.infrastructure.tags
+
 
   app_settings = {
-    "CollectionUri"                            = var.agent_ado_url
+    "CollectionUri"                            = var.infrastructure.devops.agent_ado_url
     "IS_PIPELINE_DEPLOYMENT"                   = false
-    "OVERRIDE_USE_MI_FIC_ASSERTION_CLIENTID"   = length(var.deployer.user_assigned_identity_id) > 0 ? data.azurerm_user_assigned_identity.deployer[0].client_id : null
+    "ASPNETCORE_ENVIRONMENT"                   = "PRODUCTION"
+    "OVERRIDE_USE_MI_FIC_ASSERTION_CLIENTID"   = length(var.deployer.user_assigned_identity_id) > 0 ? data.azurerm_user_assigned_identity.deployer[0].client_id : azurerm_user_assigned_identity.deployer[0].client_id
     "WEBSITE_AUTH_CUSTOM_AUTHORIZATION"        = true
     "WHICH_ENV"                                = length(var.deployer.user_assigned_identity_id) > 0 ? "DATA" : "LOCAL"
     "AZURE_TENANT_ID"                          = data.azurerm_client_config.deployer.tenant_id
@@ -115,6 +116,9 @@ resource "azurerm_windows_web_app" "webapp" {
                                                   format("@Microsoft.KeyVault(SecretUri=https://%s.privatelink.vaultcore.azure.net/secrets/PAT/)", local.keyvault_names.user_access)): (
                                                   format("@Microsoft.KeyVault(SecretUri=https://%s.vault.azure.net/secrets/PAT/)", local.keyvault_names.user_access)
                                                  )
+    "CONTROLPLANE_ENV"                        = var.infrastructure.environment
+    "CONTROLPLANE_LOC"                        = var.naming_new.location_short
+    "CONTROL_PLANE_NAME"                      = upper(format("%s-%s-%s", var.infrastructure.environment, var.naming_new.location_short, var.infrastructure.virtual_network.logical_name))
   }
 
   sticky_settings {
@@ -123,15 +127,15 @@ resource "azurerm_windows_web_app" "webapp" {
   }
 
   auth_settings_v2 {
-    auth_enabled                               = true
+    auth_enabled                               = length(var.app_service.app_registration_id) > 0
     unauthenticated_action                     = "RedirectToLoginPage"
     default_provider                           = "AzureActiveDirectory"
     active_directory_v2 {
-      client_id                                = var.app_registration_app_id
+      client_id                                = length(var.app_service.app_registration_id) > 0 ? var.app_service.app_registration_id : null
       tenant_auth_endpoint                     = "https://login.microsoftonline.com/${data.azurerm_client_config.deployer.tenant_id}/v2.0"
       www_authentication_disabled              = false
       client_secret_setting_name               = "OVERRIDE_USE_MI_FIC_ASSERTION_CLIENTID"
-      allowed_applications                     = [var.app_registration_app_id]
+      allowed_applications                     = length(var.app_service.app_registration_id) > 0 ? [var.app_service.app_registration_id] : []
       allowed_audiences                        = []
       allowed_groups                           = []
       allowed_identities                       = []
@@ -141,7 +145,9 @@ resource "azurerm_windows_web_app" "webapp" {
     }
   }
 
-  virtual_network_subnet_id = local.webapp_subnet_exists ? data.azurerm_subnet.webapp[0].id : azurerm_subnet.webapp[0].id
+
+
+  virtual_network_subnet_id = var.infrastructure.virtual_network.management.subnet_webapp.exists ? data.azurerm_subnet.webapp[0].id : azurerm_subnet.webapp[0].id
   site_config {
     # ip_restriction = [{
     #   action                    = "Allow"
@@ -164,7 +170,7 @@ resource "azurerm_windows_web_app" "webapp" {
     #                                               )
     # for now set the identity type to "SystemAssigned, UserAssigned" as assigning identities
     # is not supported by the provider when type is "SystemAssigned"
-    type                                        = "SystemAssigned, UserAssigned"
+    type                                        = "UserAssigned"
     identity_ids                                = [length(var.deployer.user_assigned_identity_id) == 0 ? azurerm_user_assigned_identity.deployer[0].id : data.azurerm_user_assigned_identity.deployer[0].id ]
                                              }
   connection_string                          {
@@ -188,15 +194,15 @@ resource "azurerm_windows_web_app" "webapp" {
 
 # Set up Vnet integration for webapp and storage account interaction
 resource "azurerm_app_service_virtual_network_swift_connection" "webapp_vnet_connection" {
-  count          = var.use_webapp ? 1 : 0
+  count          = var.app_service.use ? 1 : 0
   app_service_id = azurerm_windows_web_app.webapp[0].id
-  subnet_id      = local.webapp_subnet_exists ? data.azurerm_subnet.webapp[0].id : azurerm_subnet.webapp[0].id
+  subnet_id      = var.infrastructure.virtual_network.management.subnet_webapp.exists ? data.azurerm_subnet.webapp[0].id : azurerm_subnet.webapp[0].id
 }
 
 
 # resource "azurerm_role_assignment" "app_service_contributor" {
 #   provider             = azurerm.main
-#   count                = var.use_webapp && var.deployer.add_system_assigned_identity ? var.deployer_vm_count : 0
+#   count                = var.app_service.use && var.deployer.add_system_assigned_identity ? var.deployer_vm_count : 0
 #   scope                = azurerm_windows_web_app.webapp[0].id
 #   role_definition_name = "Website Contributor"
 #   principal_id         = azurerm_linux_virtual_machine.deployer[count.index].identity[0].principal_id
@@ -204,8 +210,41 @@ resource "azurerm_app_service_virtual_network_swift_connection" "webapp_vnet_con
 
 # resource "azurerm_role_assignment" "app_service_contributor_msi" {
 #   provider             = azurerm.main
-#   count                = var.use_webapp ? 1 : 0
+#   count                = var.app_service.use ? 1 : 0
 #   scope                = azurerm_windows_web_app.webapp[0].id
 #   role_definition_name = "Website Contributor"
 #   principal_id         = azurerm_user_assigned_identity.deployer.principal_id
+# }
+
+
+# resource "azurerm_app_service_connection" "table" {
+#   count                  = var.app_service.use && length(var.infrastructure.tfstate_resource_id) > 0 ? 1 : 0
+#   name                   = "SDAF_table_connection"
+
+#   app_service_id         = azurerm_windows_web_app.webapp[0].id
+#   target_resource_id     = var.infrastructure.tfstate_resource_id
+#   authentication {
+#     type             = "userAssignedIdentity"
+#     subscription_id  = data.azurerm_client_config.deployer.subscription_id
+#     client_id        = length(var.deployer.user_assigned_identity_id) > 0 ? data.azurerm_user_assigned_identity.deployer[0].client_id : azurerm_user_assigned_identity.deployer[0].client_id
+#   }
+#   vnet_solution          = "privateLink"
+
+# }
+
+# resource "azurerm_app_service_connection" "blob" {
+#   count                  = var.app_service.use && length(var.infrastructure.tfstate_resource_id) > 0 ? 1 : 0
+#   name                   = "SDAF_blob_connection"
+
+#   app_service_id         = azurerm_windows_web_app.webapp[0].id
+#   target_resource_id     = var.infrastructure.tfstate_resource_id
+
+#   authentication {
+#     type             = "userAssignedIdentity"
+#     subscription_id  = data.azurerm_client_config.deployer.subscription_id
+#     client_id        = length(var.deployer.user_assigned_identity_id) > 0 ? data.azurerm_user_assigned_identity.deployer[0].client_id : azurerm_user_assigned_identity.deployer[0].client_id
+#   }
+
+#   vnet_solution          = "privateLink"
+
 # }
--- deploy/terraform/terraform-units/modules/sap_deployer/output.tf
+++ deploy/terraform/terraform-units/modules/sap_deployer/output.tf
@@ -15,7 +15,7 @@ Description:
 
 output "created_resource_group_id" {
   description                          = "Created resource group ID"
-  value                                = local.resource_group_exists ? (
+  value                                = var.infrastructure.resource_group.exists ? (
                                            data.azurerm_resource_group.deployer[0].id) : (
                                            azurerm_resource_group.deployer[0].id
                                          )
@@ -23,7 +23,7 @@ output "created_resource_group_id" {
 
 output "created_resource_group_subscription_id" {
   description                          = "Created resource group' subscription ID"
-  value                                = local.resource_group_exists ? (
+  value                                = var.infrastructure.resource_group.exists ? (
                                            split("/", data.azurerm_resource_group.deployer[0].id))[2] : (
                                            split("/", azurerm_resource_group.deployer[0].id)[2]
                                          )
@@ -32,7 +32,7 @@ output "created_resource_group_subscription_id" {
 // Deployer resource group name
 output "created_resource_group_name" {
   description                          = "Created resource group name"
-  value                                = local.resource_group_exists ? (
+  value                                = var.infrastructure.resource_group.exists ? (
                                            data.azurerm_resource_group.deployer[0].name) : (
                                            azurerm_resource_group.deployer[0].name
                                          )
@@ -40,7 +40,7 @@ output "created_resource_group_name" {
 
 output "created_resource_group_location" {
   description                          = "Created resource group's location"
-  value                                = local.resource_group_exists ? (
+  value                                = var.infrastructure.resource_group.exists ? (
                                            data.azurerm_resource_group.deployer[0].location) : (
                                            azurerm_resource_group.deployer[0].location
                                          )
@@ -84,6 +84,12 @@ output "deployer_user_assigned_identity" {
   description                          = "Deployer System Assigned Identity"
   value                                = length(var.deployer.user_assigned_identity_id) > 0 ? data.azurerm_user_assigned_identity.deployer[0].principal_id : azurerm_user_assigned_identity.deployer[0].principal_id
 }
+
+output "deployer_client_id" {
+  description                          = "Deployer User Assigned Identity (Client Id)"
+  value                                = length(var.deployer.user_assigned_identity_id) > 0 ? data.azurerm_user_assigned_identity.deployer[0].client_id : azurerm_user_assigned_identity.deployer[0].client_id
+}
+
 ###############################################################################
 #                                                                             #
 #                                  Network                                    #
@@ -93,33 +99,37 @@ output "deployer_user_assigned_identity" {
 // Details of management vnet that is deployed/imported
 output "vnet_mgmt_id" {
   description                          = "Management VNet ID"
-  value                                = local.management_virtual_network_exists ? data.azurerm_virtual_network.vnet_mgmt[0].id : azurerm_virtual_network.vnet_mgmt[0].id
+  value                                = var.infrastructure.virtual_network.management.exists ? data.azurerm_virtual_network.vnet_mgmt[0].id : azurerm_virtual_network.vnet_mgmt[0].id
 }
 
 // Details of management subnet that is deployed/imported
 output "subnet_mgmt_id" {
   description                          = "Management Subnet ID"
-  value                                = local.management_subnet_exists ? data.azurerm_subnet.subnet_mgmt[0].id : azurerm_subnet.subnet_mgmt[0].id
+  value                                = var.infrastructure.virtual_network.management.subnet_mgmt.exists ? data.azurerm_subnet.subnet_mgmt[0].id : azurerm_subnet.subnet_mgmt[0].id
 }
 
 // Details of management subnet that is deployed/imported
 output "subnet_mgmt_address_prefixes" {
   description                          = "Management Subnet Address Prefixes"
-  value                                = local.management_subnet_exists ? data.azurerm_subnet.subnet_mgmt[0].address_prefixes : azurerm_subnet.subnet_mgmt[0].address_prefixes
+  value                                = var.infrastructure.virtual_network.management.subnet_mgmt.exists ? data.azurerm_subnet.subnet_mgmt[0].address_prefixes : azurerm_subnet.subnet_mgmt[0].address_prefixes
 }
 
 // Deatils of webapp subnet that is deployed/imported
 output "subnet_webapp_id" {
   description                          = "Webapp Subnet ID"
-  value                                = var.use_webapp ? (local.webapp_subnet_exists ? data.azurerm_subnet.webapp[0].id : azurerm_subnet.webapp[0].id) : ""
+  value                                = var.app_service.use ? (var.infrastructure.virtual_network.management.subnet_webapp.exists ? data.azurerm_subnet.webapp[0].id : azurerm_subnet.webapp[0].id) : ""
+}
+
+output "agent_subnet_id" {
+  description                          = "Agent Subnet ID"
+  value                                = var.infrastructure.dev_center_deployment ? (var.infrastructure.virtual_network.management.subnet_agent.exists ? data.azurerm_subnet.subnet_agent[0].id : azurerm_subnet.subnet_agent[0].id) : ""
 }
 
 // Details of the management vnet NSG that is deployed/imported
 output "nsg_mgmt" {
   description                          = "Management VNet NSG"
-  value                                = local.management_subnet_nsg_exists ? data.azurerm_network_security_group.nsg_mgmt[0] : azurerm_network_security_group.nsg_mgmt[0]
+  value                                = var.infrastructure.virtual_network.management.subnet_mgmt.nsg.exists ? data.azurerm_network_security_group.nsg_mgmt[0] : azurerm_network_security_group.nsg_mgmt[0]
 }
-
 
 output "random_id" {
   description                          = "Random ID for deployer"
@@ -128,7 +138,7 @@ output "random_id" {
 
 output "user_vault_name" {
   description                          = "Key Vault Name"
-  value                                = var.key_vault.kv_exists ? data.azurerm_key_vault.kv_user[0].name : azurerm_key_vault.kv_user[0].name
+  value                                = var.key_vault.exists ? data.azurerm_key_vault.kv_user[0].name : azurerm_key_vault.kv_user[0].name
 }
 
 ###############################################################################
@@ -140,13 +150,13 @@ output "user_vault_name" {
 // output the secret name of private key
 output "ppk_secret_name" {
   description                          = "Private Key Secret Name"
-  value                                = local.enable_key ? local.ppk_secret_name : ""
+  value                                = local.enable_key ? local.private_key_secret_name : ""
 }
 
 // output the secret name of public key
 output "pk_secret_name" {
   description                          = "Public Key Secret Name"
-  value                                = local.enable_key ? local.pk_secret_name : ""
+  value                                = local.enable_key ? local.public_key_secret_name : ""
 }
 
 output "username_secret_name" {
@@ -168,7 +178,7 @@ output "deployer_user" {
 
 output "deployer_keyvault_user_arm_id" {
   description                          = "Azure resource ID of the deployer key vault"
-  value                                = var.key_vault.kv_exists ? data.azurerm_key_vault.kv_user[0].id : azurerm_key_vault.kv_user[0].id
+  value                                = var.key_vault.exists ? data.azurerm_key_vault.kv_user[0].id : azurerm_key_vault.kv_user[0].id
 }
 
 
@@ -198,17 +208,17 @@ output "firewall_id" {
 
 output "webapp_url_base" {
   description                          = "Webapp URL Base"
-  value                                = var.use_webapp ? try(azurerm_windows_web_app.webapp[0].name, "") : ""
+  value                                = var.app_service.use ? try(azurerm_windows_web_app.webapp[0].name, "") : ""
 }
 
 output "webapp_identity" {
   description                          = "Webapp Identity"
-  value                                = var.use_webapp ? try(azurerm_windows_web_app.webapp[0].identity[0].principal_id, "") :  ""
+  value                                = var.app_service.use ? try(azurerm_windows_web_app.webapp[0].identity[0].principal_id, "") :  ""
 }
 
 output "webapp_id" {
   description                          = "Webapp ID"
-  value                                = var.use_webapp ? try(azurerm_windows_web_app.webapp[0].id, "") : ""
+  value                                = var.app_service.use ? try(azurerm_windows_web_app.webapp[0].id, "") : ""
 }
 
 ###############################################################################
@@ -232,12 +242,16 @@ output "extension_ids" {
 output "subnet_bastion_address_prefixes" {
   description                          = "Bastion Subnet Address Prefixes"
   value                                = var.bastion_deployment ? (
-                                          length(var.infrastructure.vnets.management.subnet_bastion.arm_id) == 0 ? (
-                                            azurerm_subnet.bastion[0].address_prefixes) : (
-                                            data.azurerm_subnet.bastion[0].address_prefixes
+                                          var.infrastructure.virtual_network.management.subnet_bastion.exists ? (
+                                            data.azurerm_subnet.bastion[0].address_prefixes) : (
+                                            azurerm_subnet.bastion[0].address_prefixes
                                           )) : (
                                           [""]
                                         )
 }
 
+output "diagnostics_account_id"                  {
+                                                    description = "Diagnostics Storage Account ID"
+                                                    value       = length(var.deployer.deployer_diagnostics_account_arm_id) == 0 ? azurerm_storage_account.deployer[0].id : var.deployer.deployer_diagnostics_account_arm_id
+                                                 }
 
